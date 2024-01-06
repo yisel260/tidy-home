169820b9e622721ca6945bf5b9c6c5f6b6775834
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-01-06 18:45:10 -0500

    reorganized component folders

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './Components/App.js';
 import reportWebVitals from './reportWebVitals';
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home.js";
 
 
 const router = createBrowserRouter([
