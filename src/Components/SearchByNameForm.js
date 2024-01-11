69e6029a816b2ba21c5fc28e8ca0b2233e219d20
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-01-11 09:43:25 -0500

    adds fetch request opon name search

--- src/Components/SearchByNameForm.js
+++ src/Components/SearchByNameForm.js
@@ -1,11 +1,32 @@
 import React from "react";
+import ItemCard from "./ItemCard";
 
-function handleNameSearch(event) {
-  event.preventDefault();
-  console.log("I submitted something");
-}
+
 
 function SearchByNameForm(props) {
+
+
+
+    function handleNameSearch(event) {
+        event.preventDefault();
+        console.log(event)
+
+        fetch('http://localhost:3000/items')
+        .then(res => res.json())
+       .then(itemsInfo => { 
+        itemsInfo.forEach(item => {
+          if(item.name === event.target.value ){
+            return (
+                <ItemCard />
+            )
+          }})
+        })
+    }
+
+        
+
+
+
   return (
     <div>
       <h3>Search by Name</h3>
