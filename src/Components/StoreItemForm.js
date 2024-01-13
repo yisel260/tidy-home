3dd80732005143e75df65c3c41cb19d96517ec89
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-01-13 17:49:29 -0500

    fixes synatax errors

--- src/Components/StoreItemForm.js
+++ src/Components/StoreItemForm.js
@@ -5,19 +5,20 @@ function StoreItemForm(){
 return (
 
     <form id="store-item"> 
-      <label for="item-name"> Item name:</label>
+      <label> Item name:</label>
       <input type="text" id="item-name" name = "item-name" required/> <br/>
-      <label for="room"> Room:</label>
+
+      <label> Room:</label>
       <input type="text" id="item-room" name = "item-room" required/> <br/>
 
-      <label for="location"> Location description:</label>
+      <label > Location description:</label>
       <input type="text" id="item-location" name = "item-location" required/> <br/>
 
       
-      <label for="item-category"> Category:</label>
+      <label > Category:</label>
       <input type="text" id="item-category" name = "item-category" required /><br/>
 
-      <input type="submit" value="submit" class = "button" id="submitItem"/>
+      <input type="submit" value="submit" className = "button" id="submitItem"/>
      </form>
      );
 }
