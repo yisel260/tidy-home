4bb428a7238bcee0a5d08f234add358cc6786d0e
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-01-15 08:50:59 -0500

    adds css

--- src/Components/ItemCard.js
+++ src/Components/ItemCard.js
@@ -1,14 +1,15 @@
 import react from 'react';
+import  '../pages/pages.css';
 
 function ItemCard( {itemName, itemLocation, itemRoom, itemCategory}){
 
     return (
-        <div className="ItemCard">
+        <div  className="itemCard">
             
-           Item Name: {itemName}<br />
-           Item Room: {itemRoom } <br />
-           Item Location: {itemLocation}<br />
-           Item Category: {itemCategory}  <br />    
+           <h4 className='itemTitle'>{itemName.toUpperCase()}</h4>
+           <strong/>Room<strong/>: {itemRoom } <br />
+           <p>Location: {itemLocation}</p>
+           <p>Category: {itemCategory}  </p>  
         
         </div>
 
